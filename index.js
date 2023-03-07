68596b6f51daaadc771499dd38dcc693d5a173f7
Sarabjeet Singh <ssleonardtyler@gmail.com>
2023-03-08 00:24:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+export default class Webuilder {
+    constructor(){
+
+    this.fetchRequestAccess = () => {
+        const fetchRequest = function () {
+            fetch('./data.json')
+             .then((response) => response.json())
+               .then((json) => console.log(json));
+           }
+           return fetchRequest
+    }
+}
+
+    cssAll(){
+        const i = `<link ${link()}>`;
+        console.log(i)
+    }
+
+    animationcss(){
+
+    }
+
+    navBar(){
+
+    }
+
+    searchBar(){
+
+    }
+
+    contactForm(){
+
+    }
+
+    footer(){
+
+    }
+
+    jsFrame(){
+
+    }
+}
